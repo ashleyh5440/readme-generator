649dfcedbf02f8ec2ed0aa0edbcaee8ebb36cffa
Ashley <ashleyh5440@gmail.com>
2023-11-18 14:56:53 -0500

    added files and ran test to make sure everything worked

---
+++ index.js
@@ -0,0 +1,20 @@
+// TODO: Include packages needed for this application
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    //ask questions here
+    let responses = {
+        title: "Turtle"
+    }
+    console.log(generateMarkdown(responses));
+}
+
+// Function call to initialize app
+init();
